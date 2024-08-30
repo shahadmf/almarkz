69a3f5e5dd5bb0e942d7b0072a83fe7ad65bf3cf
Shahad Alfaddagh <shahadalfaddagh@gmail.com>
2024-08-30 22:08:39 +0300

    Hero style edited

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,17 +5,15 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="background-image">
-        <div className="hero-content">
-          <h3> أسرتك متصلة، حياتك مرتبة</h3>
-          <h1>مركز القيادة لحياة عائلتك المزدحمة</h1>
-          <h4>
-            جمّع كل جداولكم ومواردكم العائلية في مكان واحد، بسلاسة ويسر، لتدير
-            منزلك الذكي بكفاءة أكبر
-          </h4>
-          <button className="start-button">
-            انشئ مركزك حالاً
-          </button>
-        </div>
+      <div className="hero-content">
+        <h3> أسرتك متصلة، حياتك مرتبة</h3>
+        <h1>مركز القيادة لحياة عائلتك المزدحمة</h1>
+        <h4>
+          جمّع كل جداولكم ومواردكم العائلية في مكان واحد، بسلاسة ويسر، لتدير
+          منزلك الذكي بكفاءة أكبر
+        </h4>
+        <button className="start-button">انشئ مركزك حالاً</button>
+      </div>
       </div>
     </div>
   );
